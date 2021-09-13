68cac3b8d060ea179dcaf80b7d406b7e7c70fb44
Beardo Mattix <christopher.mattix@gmail.com>
2021-09-13 10:24:17 -0400

    adding routes

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+// const router = require("express").Router();
+// const userRoutes = require("./userRoutes");
+// const projectRoutes = require("./projectRoutes");
+
+// router.use("/users", userRoutes);
+// router.use("/projects", projectRoutes);
+
+// module.exports = router;
+// // This needs to be updated to reflect the tech blog information.
